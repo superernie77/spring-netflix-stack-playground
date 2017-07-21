3c6116c082ec7bbe46a0ca47eb6faad6167ca7e6
superernie77 <space77>
2017-07-21 11:47:10 +0200

    Frontend implemented

---
+++ guestbook-frontend/src/main/java/com/supere77/netflix/GuestbookEntry.java
@@ -0,0 +1,57 @@
+package com.supere77.netflix;
+
+import java.util.Date;
+
+public class GuestbookEntry {
+
+	private Long id;
+	
+	private String text;
+	
+	private String title;
+	
+	private String author;
+	
+	private Date creationDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+}
