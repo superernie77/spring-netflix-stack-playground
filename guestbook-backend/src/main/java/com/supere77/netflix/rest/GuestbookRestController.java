d113227c695530fb1cd0f4ad98483c7e0b57a73f
superernie77 <space77>
2017-07-21 14:43:06 +0200

    Fixed create method bug

--- guestbook-backend/src/main/java/com/supere77/netflix/rest/GuestbookRestController.java
+++ guestbook-backend/src/main/java/com/supere77/netflix/rest/GuestbookRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class GuestbookRestController {
 	private GuestbookRepository repository;
 	
 	@RequestMapping(value = "/" , method= RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<GuestbookEntry> create(GuestbookEntry entry){
+	public ResponseEntity<GuestbookEntry> create(@RequestBody GuestbookEntry entry){
 		repository.save(entry);
 		return ResponseEntity.ok(entry);
 	}
