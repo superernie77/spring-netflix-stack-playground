6ac94897ebb6537080daa836757514291cec8281
superernie77 <space77>
2017-07-21 09:21:04 +0200

    Spring Boot Setup

---
+++ guestbook-backend/src/main/java/com/supere77/netflix/GuestbookBackendApplication.java
@@ -0,0 +1,12 @@
+package com.supere77.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookBackendApplication.class, args);
+	}
+}
