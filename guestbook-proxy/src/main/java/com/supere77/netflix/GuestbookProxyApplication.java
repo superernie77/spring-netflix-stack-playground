5466e9cdd7eb9baae54f38654ae479c873c16393
superernie77 <space77>
2017-08-05 12:32:11 +0200

    test

--- guestbook-proxy/src/main/java/com/supere77/netflix/GuestbookProxyApplication.java
+++ guestbook-proxy/src/main/java/com/supere77/netflix/GuestbookProxyApplication.java
@@ -12,6 +12,8 @@ public class GuestbookProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuestbookProxyApplication.class, args);
+	
 		
+		System.out.println("");
 	}
 }
