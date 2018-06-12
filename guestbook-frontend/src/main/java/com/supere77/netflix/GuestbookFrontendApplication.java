af44382b684e88791fc505849aeb5e51ef9525d6
superernie77 <superernie77@gmail.com>
2018-06-12 21:58:20 +0200

    added turbine config

--- guestbook-frontend/src/main/java/com/supere77/netflix/GuestbookFrontendApplication.java
+++ guestbook-frontend/src/main/java/com/supere77/netflix/GuestbookFrontendApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableTurbine
 public class GuestbookFrontendApplication {
 
 	public static void main(String[] args) {
